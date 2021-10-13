809d486c168259b32436967f5a68927ab5ee314c
zhaojiuzhou <zhaojiuzhou@ymipro.com>
2021-10-13 13:48:04 +0800

    toIP

--- iputil.go
+++ iputil.go
@@ -12,6 +12,23 @@ var (
 	errInvalidIP = errors.New("invalidIP")
 )
 
+func ToIP(s string) (ip string, err error) {
+	if !strings.Contains(s, "/") {
+		ip = net.ParseIP(s).To4().String()
+
+		return
+	}
+
+	ipc, _, err := net.ParseCIDR(s)
+	if err != nil {
+		return
+	}
+
+	ip = ipc.To4().String()
+
+	return
+}
+
 func ToCIDR(s string) (cidr string, err error) {
 	if strings.Count(s, ".") != 3 {
 		err = errNotDotIP
